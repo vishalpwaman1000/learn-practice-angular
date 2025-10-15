eb45101210b67838eef51be1591da171fdf8919f
Vishal Waman <vishalpwaman123@gmail.com>
2025-10-15 17:13:01 +0530

    Added : Menu list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,27 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { HighlightDirective } from './directive/highlight.directive';
-import { CustomColorDirectiveDirective } from './directive/custom-color-directive.directive';
+import { Router, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HighlightDirective, CustomColorDirectiveDirective],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular-learn';
+  isShowList = true;
+  topics = [
+    { topic: 'Directive', route: 'directive' },
+    { topic: 'Form', route: '' },
+    { topic: 'Pipe', route: '' },
+    { topic: 'Service', route: '' },
+    { topic: 'Component', route: '' }];
+
+  constructor(private router: Router) { }
+
+  redirectToTopic(data: any) {
+    this.isShowList = false;
+    this.router.navigate(["/" + [data.route]]);
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    // {
+    //     path: '',
+    //     loadComponent: () => import('./directive/customdirective/customdirective.component').then(m => m.CustomdirectiveComponent)
+    // },
+    {
+        path: 'directive',
+        loadComponent: () => import('./directive/customdirective/customdirective.component').then(m => m.CustomdirectiveComponent)
+    }
+];
