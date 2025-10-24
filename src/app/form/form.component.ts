2a940ffa11d3975d2ebe370be1ac0ddbab9bc7be
Vishal Waman <vishalpwaman123@gmail.com>
2025-10-24 19:20:18 +0530

    Added : error message in template driven form

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-form',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './form.component.html',
   styleUrl: './form.component.css'
 })
